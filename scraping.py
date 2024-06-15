64644f3004477e8b0d8a6f08a3b288458a209998
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2024-06-16 01:30:30 +0900

    feat: ウェブスクレイピングの機能を実装

--- scraping.py
+++ scraping.py
@@ -1,10 +1,12 @@
 import subprocess
 import re
+import requests, bs4
 
 def eprint(tline):
     tline = tline.replace('\n', '')
     param = tline.split('\t')
 
+    #文字色のパラメータ RGB値
     try:
         param[1]
 
@@ -15,6 +17,28 @@ def eprint(tline):
 
     except IndexError:
         parac = ''
+
+    #スクロール速度のパラメータ
+    try:
+        param[2]
+
+        if len(re.findall('[0-9]{1,2}', param[2])) == 0:
+            paras = ''
+        else:
+            paras = ' -s ' + param[2]
+    except IndexError:
+        paras = ''
+
+    #背景色のパラメータ
+    try:
+        param[3]
+
+        if len(re.findall('[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}', param[3])) == 0:
+            parab = ''
+        else:
+            parab = ' -B ' + param[3]
+    except IndexError:
+        parab = ''
 
     command = 'sudo'
     command += ' /home/hacku/rpi-rgb-led-matrix/examples-api-use/scrolling-text-example'
@@ -35,16 +59,37 @@ def eprint(tline):
     except:
         print('subprocess.check_call() faild')
 
+def news():
+    res = requests.get('https://www3.nhk.or.jp/news/catnew.html')
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.content, "html.parser")
+    elems = soup.select('em.title')
+
+    l = 0
+
+    for elem in elems:
+        nwt = '〜NEWS〜 '
+        nwt += elem.getText().replase('\n', '') + '\t255,255,255\t2'
+        eprint(nwt)
+        l += 1
+        if l > 2:
+            break
+
 def main():
     #テキストファイル読み込み
     f = open('display.txt', 'r', encoding='UTF-8')
     while True:
         line = f.readline()
+
         if line:
-            eprint(line)
+            if '[NEWS]' in line:
+                news()
+            else:
+                eprint(line)
         else:
             break
     f.close()
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
